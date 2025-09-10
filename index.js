7d6854c257f615edbd65692c1d6d3ffc0f17f820
Mitu-Rani-Sutradhar <mitusutradhar4965@gmail.com>
2025-09-10 12:15:00 +0600

    added modal part

--- index.js
+++ index.js
@@ -31,15 +31,44 @@ const displayCategory = (categories) => {
 	}
 };
 
+
+const loadTreeDetail =async(id) =>{
+    const url =`https://openapi.programming-hero.com/api/plant/${id}`;
+    const res =await fetch(url);
+    const details = await res.json();
+    displayTreeDetails(details.plants);
+}
+
+displayTreeDetails =(plant) =>{
+    console.log(plant);
+    const detailsBox = document.getElementById("details-container");
+    detailsBox.innerHTML =
+    `
+    <div class="p-4 w-[320px] h-[450px] bg-white rounded-xl mb-3">
+
+                <h3 = class="font-bold py-2">${plant.name}</h3> 
+
+                <div class="h-[180px] w-full">
+                   <img class="h-[180px] w-full" src="${plant.image}" alt="">
+                   </div>
+                    <p class=" h-[22px] w-[200px] font-bold mb-3 mt-3">Category:${plant.category}</p>
+                     <p class="pr-5 font-bold">Price: ৳${plant.price}</p>
+                     <p class="text-regular h-[120px] font-bold mt-3">Description: <br> ${plant.description}</p>
+                </div> 
+    `;
+    document.getElementById("my_modal_5").showModal();
+}
+
+
 const loadCategoryPlants = (id) => {
    manageSpinner(true);
-
-	const url = `https://openapi.programming-hero.com/api/category/${id}`
+const url = `https://openapi.programming-hero.com/api/category/${id}`
 	fetch(url)
 		.then((res) => res.json())
 		.then((data) => {
 			displayCategoryTree(data.plants)
 		});
+
 
         const displayCategoryTree =(trees) => {
         //   console.log(trees)};
@@ -57,7 +86,7 @@ const loadCategoryPlants = (id) => {
 // }
           
             for(const tree of trees){
-                console.log(tree);
+                // console.log(tree);
                 const card = document.createElement("div");
                 card.innerHTML=`
                 <div class="p-4 w-[320px] h-[450px] bg-white rounded-xl mb-3">
@@ -65,13 +94,13 @@ const loadCategoryPlants = (id) => {
                     <img class="h-[180px] w-[290px]" src="${tree.image}" alt="">
                         
                     </div>
-                    <h3 class="font-bold py-2">${tree.name}</h3>
+                    <h3 onclick="loadTreeDetail(${tree.id})" class="font-bold py-2">${tree.name}</h3>
                     <p class="text-regular h-[120px]">${tree.description}</p>
                     <div class="flex justify-between py-3">
                         <p class="bg-[#A4FDC5] h-[22px] w-[200px] text-center rounded-2xl">${tree.category}</p>
                         <p class="pr-5">৳${tree.price}</p>
                     </div>
-                    <button class="bg-[#15803D] w-full h-[30px] text-white rounded-2xl">Add to Cart</button>
+                    <button class="bg-[#15803D] w-full h-[30px] text-white rounded-2xl cart-btn">Add to Cart</button>
                 </div> 
                 
                 `;  
@@ -87,6 +116,5 @@ const loadCategoryPlants = (id) => {
 };
 
 loadCategories();
-
 
 
